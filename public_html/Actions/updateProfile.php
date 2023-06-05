e423ff7c8d352ce290544f05ef9eb906a19a9771
keccc <vishalkrsinghmsk@gmail.com>
2023-06-05 15:26:45 +0530

    update profile

---
+++ public_html/Actions/updateProfile.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+
+$user_id=getUserFromToken($post["token"])["id"];
+
+
+$username  = $post["username"];
+$name = $post["name"];
+$password = $post["password"];
+$exam = $post["exam"];
+
+$sql = "update user set email = '".$username."' and name = '".$name."' and password = '".$password."' and exam='".$exam."' where id=".$user_id;
+
+$result = mysqli_query($conn , $sql);
+
+if($result){
+    ResponseGenerator(1,$post["token"],"Your Profile is successfully updated  ");
+}else{
+    ResponseGenerator(0,$post["token"]," Failed to update your profile. Kindly try again later ");
+}
--- public_html/include/conn.php
+++ public_html/include/conn.php
@@ -1,6 +1,6 @@
 <?php
-//$conn  = mysqli_connect("127.0.0.1" , "root" , "" , "testme");
-$conn  = mysqli_connect("localhost" , "id17903953_root" , "(ZZ@@Q*_LWtO1k{X" , "id17903953_testme");
+$conn  = mysqli_connect("127.0.0.1" , "root" , "" , "test");
+//$conn  = mysqli_connect("localhost" , "id17903953_root" , "(ZZ@@Q*_LWtO1k{X" , "id17903953_testme");
 $post = json_decode(file_get_contents("php://input"),true);
 
 include_once "lib.php";
